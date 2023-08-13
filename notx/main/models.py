5c0f5cdba2e82c61e70d1f9836c0b278fbc0b86d
Success Aje <successaje7@gmail.com>
2023-08-13 12:32:05 +0100

    initializing the app

--- notx/main/models.py
+++ notx/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+import rules
 
-# Create your models here.
+
+
+class Alert(models.Model):
+    title = models.CharField(max_length=200, default = "Alert")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    product_name = models.CharField(max_length=200)
+    expiry_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ["expiry_date"]
+
+    def __str__(self):
+        return f"{self.title} at {self.time}"
+    def get_absolute_url(self):
+        return "/"
+    
