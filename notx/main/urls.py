26361cb49bd4f19c5b064bad4604f38f9048b403
Success Aje <successaje7@gmail.com>
2023-08-22 03:44:32 +0100

    Update urls.py
    urls patterns update
    
    Co-Authored-By: MainSolarCoder <139449138+MainSolarCoder@users.noreply.github.com>

--- notx/main/urls.py
+++ notx/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+    path('', views.AlertListAPIView.as_view(), name = "alerts"),
+    path('<int:id>', views.AlertDetailAPIView.as_view(), name = "alert"),
 ]
